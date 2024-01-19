a9a462d5423d417dea28b9cb2d7b4b6637f3ce27
Reut Vladislav <reut-2011@mail.ru>
2024-01-19 16:26:43 +0300

    feat: add second part (full functionality) in 03-files-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,13 +6,25 @@ const pathFolder = path.join(__dirname, 'secret-folder');
 async function readDirectory() {
   try {
     const files = await fs.readdir(pathFolder, { withFileTypes: true });
-    const fileNames = [];
     for (let file of files) {
       if (file.isFile()) {
-        fileNames.push(file.name);
+        let pathFile = path.join(pathFolder, file.name);
+
+        // Метод предоставляющий расширение файла по его директории
+        let extName = path.extname(file.name).slice(1);
+
+        // Метод предоставляющий имя файла по его директории и расширению
+        const nameFile = path.basename(pathFile, path.extname(file.name));
+
+        // Метод предоставляющий информацию о файле по его директории
+        let stats = await fs.stat(pathFile);
+        let fileSizeB = stats.size; // Размер файла в байтах
+        const fileSizeKB = (stats.size / 1024).toFixed(3); // Размер файла в килобайтах
+        console.log(
+          `${nameFile} - ${extName} - ${fileSizeB}B - ${fileSizeKB}KB`,
+        );
       }
     }
-    console.log(fileNames);
   } catch (error) {
     console.error(error);
   }
